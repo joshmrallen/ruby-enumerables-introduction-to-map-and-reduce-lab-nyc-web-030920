d4a543cee9d09f0fc73f5fd7c0c9263ee547ca99
Josh Allen <joshmrallen@gmail.com>
2020-02-28 15:00:29 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,2 +1,13 @@
 # My Code here....
 
+def map_to_negativize(source_array)
+  
+  index = 0
+  while index < source_array.count do
+    source_array[index] *= -1
+    index += 1
+  end
+  return source_array
+
+end
+
