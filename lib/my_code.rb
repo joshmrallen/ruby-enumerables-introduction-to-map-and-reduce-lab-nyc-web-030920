a91d7983ac72943532798f0fee00817bfebac78e
Josh Allen <joshmrallen@gmail.com>
2020-02-28 15:06:05 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -22,6 +22,22 @@ def map_to_no_change(source_array)
 end
 
 def map_to_double(source_array)
+  index = 0
+  while index < source_array.count do
+    source_array[index] *= 2
+    index += 1
+  end
+  return source_array
+end
+
+def map_to_square(source_array)
+  index = 0
+  while index < source_array.count do
+    source_array[index] *= source_array[index]
+    index += 1
+  end
+  return source_array
+  
   
   
   
